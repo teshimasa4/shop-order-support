ecd7877e8e109389c4ebae341ac41158f0ae1c65
teshimasa4 <teshimasa4@yahoo.co.jp>
2017-08-07 23:11:00 +0900

    security追加。

--- WebContent/WEB-INF/views/index.jsp
+++ WebContent/WEB-INF/views/index.jsp
@@ -1,10 +1,10 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-</head>
-<body>
-	Login
-</body>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    Security!!${welcomeMesssage}
+  </body>
 </html>
--- src/main/java/jp/xxxxx/shop_order_support/controller/IndexController.java
+++ src/main/java/jp/xxxxx/shop_order_support/controller/IndexController.java
@@ -3,14 +3,16 @@ package jp.xxxxx.shop_order_support.controller;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class LoginController {
+public class IndexController {
 
-	@RequestMapping(value = "/login", method = GET)
-	public String home() {
-		return "login";
+	@RequestMapping(value = "/", method = GET)
+	public String home(Model model) {
+		model.addAttribute("welcomeMesssage", "Hello!!");
+		return "index";
 	}
 }
