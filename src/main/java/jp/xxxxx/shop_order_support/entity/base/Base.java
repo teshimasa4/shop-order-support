a6fb640bc08dea32eb68e42d78aa53c1ef2dd069
teshimasa4 <teshimasa4@yahoo.co.jp>
2017-08-30 00:49:36 +0900

    Serializable。

--- src/main/java/jp/xxxxx/shop_order_support/entity/base/Base.java
+++ src/main/java/jp/xxxxx/shop_order_support/entity/base/Base.java
@@ -1,11 +1,15 @@
 package jp.xxxxx.shop_order_support.entity.base;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
 import lombok.Data;
 
 @Data
-public class Base {
+public class Base implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private Integer versionNo;
 	private String createUserCd;
 	private Timestamp createTime;
